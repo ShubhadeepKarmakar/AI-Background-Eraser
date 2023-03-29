249a2bd2e7ed78c03ba1bd02d01b603a8ae9692c
Shubhadeep_Karmakar <shubhadeepkarmakar1@gmail.com>
2023-03-29 16:16:22 +0530

    empty

---
+++ app/build/generated/source/buildConfig/debug/com/example/ai_background_eraser/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.ai_background_eraser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.ai_background_eraser";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
